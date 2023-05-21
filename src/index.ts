1efa87030007a7603d2ba687db77f0bf71ac7220
robertocaliz <robertocaliz72@gmail.com>
2023-05-21 13:24:15 +0200

    feat: function to run migrations when start server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,30 @@
+import { Knex } from './server/database/knex';
 import { server } from './server/server';
 
 
-server.listen(process.env.PORT || 3333, () => {
-  console.log(`Server running in port ${process.env.PORT}`);
-});
+
+
+const startServer = () => {
+  server.listen(process.env.PORT || 3333, () => {
+    console.log(`Server running in port ${process.env.PORT}`);
+  });
+};
+
+
+
+const runMigrations = async () => {
+  if (process.env.IS_LOCALHOST !== 'true') {
+    await Knex.migrate.latest()
+      .then(() => {
+        startServer();
+      })
+      .catch(console.log);
+    return;
+  }
+  startServer();
+};
+
+runMigrations();
+
+
+
